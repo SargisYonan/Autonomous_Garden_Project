3b658b87d25200554e72cb76ed8ccd117c259e4a
legendddhgf <legendddhgf@gmail.com>
2016-01-29 20:44:22 -0800

    started a new h file

---
+++ src/firmware/command_library.h
@@ -0,0 +1,34 @@
+#ifndef _COMMAND_LIBRARY_H_
+#define _COMMAND_LIBRARY_H_
+
+uint8_t getStatus();
+
+uint8_t sysEnable();
+
+uint8_t sysDisable();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //_COMMAND_LIBRARY_H_
