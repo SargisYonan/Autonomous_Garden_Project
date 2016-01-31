ab3b66adce688fc170b950326f5dcb81ce2c9639
legendddhgf <legendddhgf@gmail.com>
2016-01-30 20:30:47 -0800

    Added timers

---
+++ src/firmware/SYSCLK/sysclk.c
@@ -0,0 +1,53 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "../STATE_MACHINE/finite_state_machine.h"
+#include "sysclk.h"
+
+int count;
+
+void idle_hours(int hours) {
+
+  cli();
+
+  SystemStatus->state = IDLE;
+
+  count = (hours * 60 * 60 * 16000); // 16000 is because of clock speed
+
+  TCCR1B |= (1 << CS10) | (1 << CS12); // clk/1024 prescaler
+
+  TCCR1A = 0;    // set entire TCCR1A register to 0
+
+  TCCR1B = 0;    // set entire TCCR1B register to 0
+
+  sei();
+
+}
+
+void idle_mins(int mins) {
+
+  cli();
+
+  SystemStatus->state = IDLE;
+
+  count = (mins * 60 * 16000); // 16000 is because of clock speed
+
+  TCCR1B |= (1 << CS10) | (1 << CS12); // clk/1024 prescaler
+
+  TCCR1A = 0;    // set entire TCCR1A register to 0
+
+  TCCR1B = 0;    // set entire TCCR1B register to 0
+
+  sei();
+
+}
+
+ISR(TIMER1_OVF_vect) {
+
+  TCCR1B |= (1 << CS10) | (1 << CS12); // clk/1024 prescaler
+
+  if (--count <= 0) {
+    SystemStatus->state = VALVE_CLOSED;
+    TCCR1B = 0
+  }
+
+}
---
+++ src/firmware/SYSCLK/sysclk.h
@@ -0,0 +1,9 @@
+#ifndef _SYSCLK_H_
+#define _SYSCLK_H_
+
+void idle_hours(int hours);
+
+void idle_mins(int mins);
+
+#endif //_SYSCLK_H_
+
