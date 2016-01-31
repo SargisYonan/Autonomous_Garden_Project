8ebdd9fd6653956c5d191c4438d3a29f3c7365d9
legendddhgf <legendddhgf@gmail.com>
2016-01-31 00:43:17 -0800

    silly fixes

--- src/firmware/SERVO/servo.c
+++ src/firmware/SERVO/servo.c
@@ -18,12 +18,12 @@ void changeServoState(uint8_t state)
   TCCR2B = 0;    // set entire TCCR2B register to 0
   TCCR2B |= (1 << CS10); // no prescaler
   TIMSK2 |= (1 << TOIE0);
-  TCNT2= (servo_state == SERVO_OPEN ? 0x0612 : 0x048E); // this is the timer
+  TCNT2= (servo_state->state == SERVO_OPEN ? 0x0612 : 0x048E); // this is the timer
   sei();
 }
 
 ISR(TIMER2_OVF_vect) 
 {
   SERVO_PORT ^= SERVO_ON_SETTING;
-  TCNT2= (servo_state == SERVO_OPEN ? 0x0612 : 0x048E); // this is the timer
+  TCNT2= (servo_state->state == SERVO_OPEN ? 0x0612 : 0x048E); // this is the timer
 }
