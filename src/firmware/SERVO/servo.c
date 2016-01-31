21361eebe7b4b4be4d9733a6c0b6fe119182bbf0
legendddhgf <legendddhgf@gmail.com>
2016-01-31 04:11:18 -0800

    More attempts

--- src/firmware/SERVO/servo.c
+++ src/firmware/SERVO/servo.c
@@ -33,54 +33,53 @@ void ChangeServoState(uint8_t state)
   TCCR3B = 0;    // set entire TCCR1B register to 0
   TCCR3B |= (1 << CS11); // clk/8 prescaler
   TIMSK3 |= (1 << TOIE1);
-  TCNT3 = !0; // this is the timer
+  TCNT3 = 1000; // this should effectively make a .5 ms ISR
   SERVO_PORT &= 0x00;
   SERVO_PORT |= SERVO_ON_SETTING;
   sei();
 
   /*
-  cli();
-  freq  = 0;
-  TCCR2A = 0;    // set entire TCCR2A register to 0
-  TCCR2B = 0;    // set entire TCCR2B register to 0
-  TCCR2B |= (1 << CS10); // no prescaler
-  TIMSK2 |= (1 << TOIE0);
-  TCCR3A = 0;    // set entire TCCR0A register to 0
-  TCCR3B = 0;    // set entire TCCR0B register to 0
-  TCCR3B |= (1 << CS10); // no prescaler
-  TIMSK3 |= (1 << TOIE0);
-  servo_state->duty_count = (servo_state->state == SERVO_OPEN ? 10 : 13);
-  servo_state->state = state;
-  servo_state->duty_cycle_state = SERVO_DUTY_ON;
-  TCNT2 = ((servo_state->state == SERVO_OPEN) ? 0x0612 : 0x048E); // this is the timer
-  TCNT3 = 0xF2E1;
-  sei();
-  */
+     cli();
+     freq  = 0;
+     TCCR2A = 0;    // set entire TCCR2A register to 0
+     TCCR2B = 0;    // set entire TCCR2B register to 0
+     TCCR2B |= (1 << CS10); // no prescaler
+     TIMSK2 |= (1 << TOIE0);
+     TCCR3A = 0;    // set entire TCCR0A register to 0
+     TCCR3B = 0;    // set entire TCCR0B register to 0
+     TCCR3B |= (1 << CS10); // no prescaler
+     TIMSK3 |= (1 << TOIE0);
+     servo_state->duty_count = (servo_state->state == SERVO_OPEN ? 10 : 13);
+     servo_state->state = state;
+     servo_state->duty_cycle_state = SERVO_DUTY_ON;
+     TCNT2 = ((servo_state->state == SERVO_OPEN) ? 0x0612 : 0x048E); // this is the timer
+     TCNT3 = 0xF2E1;
+     sei();
+   */
 }
 
 ISR(TIMER3_OVF_vect) 
 {
-  if (ISR_COUNT == 100) {
-    ISR_COUNT = 0;
-  if (SERVO_TIME >= 0) SERVO_TIME++;
-  if (SERVO_COUNT == 40)
-  {
-    SERVO_PORT |= SERVO_ON_SETTING;
-    SERVO_COUNT = 0;
-    SERVO_TIME = 0;
-  }
-  else SERVO_COUNT++;
+  //if (ISR_COUNT == 1000) {
+    //ISR_COUNT = 0;
+    if (SERVO_TIME >= 0) SERVO_TIME++;
+    if (SERVO_COUNT == 40)
+    {
+      SERVO_PORT |= SERVO_ON_SETTING;
+      SERVO_COUNT = 0;
+      SERVO_TIME = 0;
+    }
+    else SERVO_COUNT++;
 
 
-  if (SERVO_COUNT_TWO == SERVO_TIME) 
+    if (SERVO_COUNT_TWO == SERVO_TIME) 
     {
       SERVO_PORT &= 0x00;
       SERVO_TIME = -1; 
     }
-  //TCNT3=0x0; // this is the timer
 
-} else {
-  ISR_COUNT++;
-}
-  TCNT3 = !0;
+  //} else {
+  //  ISR_COUNT++;
+ // }
+  TCNT3 = 1000;
 }
